7660910bbad699d955a942fb54c1ffe83251f310
Ruben Valdez <ruben.valdez7@gmail.com>
2019-09-22 23:29:49 -0700

    add banner image

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+
 function Jumbotron({ children }) {
   return (
     <div
-      style={{ textAlign: "center" }}
+      style={{ textAlign: "center",
+              backgroundImage:"url('https://i.imgur.com/5rbTbyz.jpg')",
+              color:"maroon",
+            fontWeight:"bolder" }}
       className="col-lg jumbotron"
     >
       {children}
